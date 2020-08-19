ece173f373c969de5accf2fea3ba5cba6f018092
Lebo <mphagol@gmail.com>
2020-08-19 17:11:39 +0200

    updated after review

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,25 +1,42 @@
 //index_spec.js
-let String = require("../index")
-describe("String Calculator", function() {
+  
+var {Calculator} = require("./src/index")
 
 
-    it ("should return zero when '' is passed", function() {
-        expect(String('')).toEqual(0);
-    });
-
-    it ("should return the number itself when a single number is passed", function() {
-        expect(String.add('1')).toEqual(1);
-    });
-
-    it ("should return the sum of the number if two are given", function() {
-        expect(String.add('1,1')).toEqual(2);
-    });
-
-    it("should allow \\n in between the input number string", function() {
-        expect(String.add("1\n2,3")).toEqual(6);
-    });
-
-
- });
+describe("String calculator should return zero when '' is passed", function () {
+    let calculator = new Calculator();
+ 
+     it(" String calculator should return equal to zero", function () {
+         expect(calculator.add("")).toEqual(0);
+     });
+  
+     it("String calculator should return equal  to one", function () {
+         expect(calculator.add("1")).toEqual(1);
+     });
+ 
+     it("String calculator should return equal 3", function () {
+         expect(calculator.add("1,2")).toEqual(3);
+     });
+ 
+     it("String calculator should separate numbers with \\n", function () {
+       expect(calculator.add("1\n2\n3")).toEqual(6);
+     });
+ 
+     it("String calculator should throw an exception that doesnt allow negative numbers", () => {
+       expect(() => {calculator.add("-5,-1")}).toThrowError('Negative not allowed: -5 -1 ');
+         });
+ 
+     it("String calculator should ignore numbers greater or equal to 1000", () => {
+       expect(calculator.add("1000,1")).toEqual(1);
+         });
+ 
+     it("String calculator should have multiple delimiter of length > 1", function () {
+       expect(calculator.add("//[***]\n1***2***3")).toEqual(6);
+     });
+ 
+     it("String calculator should have more than one separators, which can be defined", function () {
+       expect(calculator.add("//[*][%]\n1*2%3")).toEqual(6);
+     });
+   });
     
     
---
+++ spec/src/index.js
@@ -0,0 +1,46 @@
+class Calculator{
+
+    negatives(string) {
+		let str = '';
+		let numbers = string.split(',');
+
+		for (var i = 0; i < numbers.length; i++) {
+			if (numbers[i][0] == '-') {
+				str += numbers[i] +' ';
+			}
+		}
+
+		return str;
+	}
+
+    add(numbers) {
+
+		let results = 0;
+		let negatives = this.negatives(numbers);
+
+		numbers = numbers.split(/\D/);
+
+		if (negatives) {
+			throw new Error('Negative not allowed: '+ negatives);
+	   	}	
+
+		for (let i = 0; i < numbers.length; i++) {
+			
+			let number = parseInt(numbers[i]); 
+
+		 	if (number >= 1000) {
+		 	  number = 0;
+			}
+			
+		 	if (number) {
+		 		
+				results += number;
+			}
+		}
+
+		return results;
+	}
+      
+}
+
+ module.exports = {Calculator}
